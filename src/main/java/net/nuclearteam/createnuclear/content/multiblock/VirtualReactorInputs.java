4cf9cb4092c82320146badf07bc0f1587cfcec47
Giovanni <giovanni.ricotta@ynov.com>
2026-01-04 17:20:40 +0100

    refactor: simplify VirtualReactorInputs by removing ItemStackHandler and using BigItemStack for fuel and cooler representation

--- src/main/java/net/nuclearteam/createnuclear/content/multiblock/VirtualReactorInputs.java
+++ src/main/java/net/nuclearteam/createnuclear/content/multiblock/VirtualReactorInputs.java
@@ -1,80 +1,25 @@
 package net.nuclearteam.createnuclear.content.multiblock;
 
-import net.minecraft.nbt.CompoundTag;
-import net.minecraft.world.item.ItemStack;
-import net.minecraftforge.items.IItemHandler;
-import net.minecraftforge.items.ItemStackHandler;
+import com.simibubi.create.content.logistics.BigItemStack;
 import net.nuclearteam.createnuclear.CNItems;
-import net.nuclearteam.createnuclear.CNTags.CNItemTags;
-import net.nuclearteam.createnuclear.content.multiblock.controller.manager.ReactorInputManagerI;
 import org.jetbrains.annotations.NotNull;
 
-public class VirtualReactorInputs extends ItemStackHandler {
-    public VirtualReactorInputs(ReactorInputManagerI input) {
-        super(2 * (input.size() <= 0 ? 1 : input.size()));
+public record VirtualReactorInputs(int fuel, int cooler) {
+    public VirtualReactorInputs() {
+        this(0,0);
     }
 
-    public void clear() {
-        for (int i = 0; i < getSlots(); i++)
-            setStackInSlot(i, ItemStack.EMPTY);
+    public BigItemStack getBigFuelRod() {
+        return new BigItemStack(CNItems.URANIUM_ROD.asStack(), fuel);
     }
 
-    public boolean isEmpty() {
-        for (int i = 0; i < getSlots(); i++)
-            if (!getStackInSlot(i).isEmpty())
-                return false;
-        return true;
+    public BigItemStack getBigCooledRod() {
+        return new BigItemStack(CNItems.GRAPHITE_ROD.asStack(), cooler);
     }
 
-    public @NotNull ItemStack insertFuelItem(ItemStack stack, boolean simulate) {
-        return insertItem(0, stack, simulate);
-//        if (stack.is(CNItemTags.FUEL.tag))
-//        return ItemStack.EMPTY;
-    }
-
-    public @NotNull ItemStack insertCoolerItem(ItemStack stack, boolean simulate) {
-        return insertItem(1, stack, simulate);
-//        if (stack.is(CNItemTags.COOLER.tag))
-//        return ItemStack.EMPTY;
-    }
-
-    public ItemStack getFuelSlot() {
-        return getStackInSlot(0);
-    }
-
-    public ItemStack getCoolerSlot() {
-        return getStackInSlot(1);
-    }
-
-    public record VirtualReactorInputsR(int fuel, int cooler) {
-        public VirtualReactorInputsR() {
-            this(0,0);
-        }
-
-        public ItemStack getFuelRod() {
-            return new ItemStack(CNItems.URANIUM_ROD.asItem(), fuel);
-        }
-
-        public ItemStack getCooledRod() {
-            return new ItemStack(CNItems.GRAPHITE_ROD.asItem(), cooler);
-        }
-
-        @Override
-        public @NotNull String toString() {
-            return "fuel: " + fuel + " cooler: " + cooler;
-        }
-
-        public void write(CompoundTag tag) {
-            CompoundTag nbt = new CompoundTag();
-            nbt.putInt("fuel", fuel);
-            nbt.putInt("cooler", cooler);
-            tag.put("reactorInventory", nbt);
-        }
-
-        public VirtualReactorInputsR read(CompoundTag tag) {
-            CompoundTag nbt = tag.getCompound("reactorInventory");
-
-            return new VirtualReactorInputsR(nbt.getInt("fuel"), nbt.getInt("cooler"));
-        }
+    @Override
+    public @NotNull String toString() {
+        return "fuel: " + fuel + " cooler: " + cooler;
     }
 }
+
