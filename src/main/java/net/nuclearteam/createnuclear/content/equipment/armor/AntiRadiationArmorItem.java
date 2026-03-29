0ef15a9e92f5f899bb3f71d55307d8aa27e26a05
Giovanni <giovanni.ricotta@ynov.com>
2026-03-29 19:35:26 +0200

    feat: Enhance anti-radiation armor item with improved texture retrieval and tooltip summary

--- src/main/java/net/nuclearteam/createnuclear/content/equipment/armor/AntiRadiationArmorItem.java
+++ src/main/java/net/nuclearteam/createnuclear/content/equipment/armor/AntiRadiationArmorItem.java
@@ -1,13 +1,25 @@
 package net.nuclearteam.createnuclear.content.equipment.armor;
 
+import com.simibubi.create.AllKeys;
+import net.minecraft.ChatFormatting;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.chat.CommonComponents;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.item.*;
+import net.minecraft.world.level.Level;
 import net.minecraftforge.client.extensions.common.IClientItemExtensions;
-import net.nuclearteam.createnuclear.CNTags;
+import net.minecraftforge.registries.ForgeRegistries;
 import net.nuclearteam.createnuclear.CreateNuclear;
+import net.nuclearteam.createnuclear.content.equipment.cloth.ClothItem;
+import net.nuclearteam.createnuclear.foundation.util.ClothTagHelper;
+import net.nuclearteam.createnuclear.foundation.utility.CreateNuclearLang;
+import org.jetbrains.annotations.Nullable;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.function.Consumer;
 
 public abstract class AntiRadiationArmorItem extends ArmorItem {
@@ -22,11 +34,36 @@ public abstract class AntiRadiationArmorItem extends ArmorItem {
     // Cette méthode indique au jeu où trouver la texture PNG de l'armure
     @Override
     public String getArmorTexture(ItemStack stack, Entity entity, EquipmentSlot slot, String type) {
-        CompoundTag tag = stack.getTag();
-        String cloth = (tag != null && tag.contains("Cloth")) ? tag.getString("Cloth") : "default";
-        String textureFile = cloth + "_anti_radiation_suit.png";
-        return CreateNuclear.asResource("textures/models/armor/" + textureFile).toString();
+        return ClothTagHelper.getArmorTexturePath(stack, "anti_radiation_suit.png");
     }
+
+    @Override
+    public void appendHoverText(ItemStack pStack, @Nullable Level pLevel, List<Component> tooltip, TooltipFlag pIsAdvanced) {
+        if (AllKeys.shiftDown()) return;
+
+        List<Component> makeSummary = makeSummary(pStack);
+        if (makeSummary.isEmpty()) return;
+
+        tooltip.add(CommonComponents.SPACE);
+        tooltip.addAll(makeSummary);
+    }
+
+    public List<Component> makeSummary(ItemStack item) {
+        if (!item.hasTag()) return Collections.emptyList();
+
+        CompoundTag tag = item.getTag();
+        ItemStack cloth = ItemStack.of(tag.getCompound(ClothTagHelper.ITEM));
+        if (!(cloth.getItem() instanceof ClothItem clothItem)) return Collections.emptyList();
+
+        List<Component> list = new ArrayList<>();
+        CreateNuclearLang.translate("tooltip.cloth.color", clothItem.getColor().getSerializedName())
+            .color(clothItem.getColor().getTextColor())
+            .style(ChatFormatting.ITALIC)
+            .addTo(list);
+
+        return list;
+    }
+
 
     @Override
     public void initializeClient(Consumer<IClientItemExtensions> consumer) {
