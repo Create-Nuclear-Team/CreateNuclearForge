84d5fb7f2df2fdc19cc1f9455e2755ca0d163083
Giovanni <giovanni.ricotta@ynov.com>
2026-03-29 19:36:22 +0200

    feat: Remove ArmorClothsDescription from tooltip modifier for cleaner item descriptions

--- src/main/java/net/nuclearteam/createnuclear/CreateNuclear.java
+++ src/main/java/net/nuclearteam/createnuclear/CreateNuclear.java
@@ -25,7 +25,6 @@ import net.minecraftforge.registries.RegisterEvent;
 import net.nuclearteam.createnuclear.content.contraptions.irradiated.IrradiatedAnimal;
 import net.nuclearteam.createnuclear.content.decoration.palettes.CNPaletteBlocks;
 import net.nuclearteam.createnuclear.content.kinetics.fan.processing.CNFanProcessingTypes;
-import net.nuclearteam.createnuclear.foundation.item.ArmorClothsDescription;
 import net.nuclearteam.createnuclear.foundation.item.RodsStats;
 import net.nuclearteam.createnuclear.infrastructure.config.CNConfigs;
 import net.nuclearteam.createnuclear.infrastructure.data.CreateNuclearDatagen;
@@ -41,7 +40,7 @@ public class CreateNuclear {
 
     static {
         REGISTRATE.setTooltipModifierFactory(item -> new ItemDescription.Modifier(item, FontHelper.Palette.STANDARD_CREATE)
-                .andThen(TooltipModifier.mapNull(KineticStats.create(item))).andThen(RodsStats.create(item)).andThen(ArmorClothsDescription.create(item)));
+            .andThen(TooltipModifier.mapNull(KineticStats.create(item))).andThen(RodsStats.create(item)));
     }
 
     public CreateNuclear() {
