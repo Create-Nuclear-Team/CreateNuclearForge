2e67c369a859437b21788f8d6692c38ec295bf22
Giovanni <giovanni.ricotta@ynov.com>
2026-04-12 11:26:14 +0200

    [WIP] Refactor LiquidLevelDisplaySource to enhance fluid display logic and improve localization handling

--- src/main/java/net/nuclearteam/createnuclear/content/redstone/displayLink/source/LiquidLevelDisplaySource.java
+++ src/main/java/net/nuclearteam/createnuclear/content/redstone/displayLink/source/LiquidLevelDisplaySource.java
@@ -3,16 +3,28 @@ package net.nuclearteam.createnuclear.content.redstone.displayLink.source;
 import com.simibubi.create.content.redstone.displayLink.DisplayLinkContext;
 import com.simibubi.create.content.redstone.displayLink.source.NumericSingleLineDisplaySource;
 import com.simibubi.create.content.redstone.displayLink.target.DisplayTargetStats;
+import net.createmod.catnip.lang.LangBuilder;
+import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.MutableComponent;
+import net.nuclearteam.createnuclear.CreateNuclear;
+import net.nuclearteam.createnuclear.content.logistics.BigFluidStack;
 import net.nuclearteam.createnuclear.content.multiblock.controller.ReactorControllerBlockEntity;
 import net.nuclearteam.createnuclear.foundation.utility.CreateNuclearLang;
 
 public class LiquidLevelDisplaySource extends NumericSingleLineDisplaySource {
     @Override
     protected MutableComponent provideLine(DisplayLinkContext context, DisplayTargetStats stats) {
-        if (!(context.getSourceBlockEntity() instanceof ReactorControllerBlockEntity controller)) return ZERO.copy();
+        if (!(context.getSourceBlockEntity() instanceof ReactorControllerBlockEntity controller) || controller.getBigFluidStack().isEmpty()) return ZERO.copy();
 
-        return CreateNuclearLang.translate("generic.unit.liquid").component();
+        BigFluidStack stack = controller.getBigFluidStack().get(0);
+
+        LangBuilder builder = CreateNuclearLang.builder(CreateNuclear.MOD_ID).translate("generic.unit.liquid");
+
+        builder.add(stack.stack.getDisplayName());
+
+
+
+        return builder.component();
     }
 
     @Override
