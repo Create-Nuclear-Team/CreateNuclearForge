bf4a9794dc5bba17e05fa3c3e56617d94373fc31
Giovanni <giovanni.ricotta@ynov.com>
2026-05-10 19:45:44 +0200

    feat: Add entity blacklist configuration to CRadiation for radiation effects

--- src/main/java/net/nuclearteam/createnuclear/infrastructure/config/CRadiation.java
+++ src/main/java/net/nuclearteam/createnuclear/infrastructure/config/CRadiation.java
@@ -2,10 +2,18 @@ package net.nuclearteam.createnuclear.infrastructure.config;
 
 import net.createmod.catnip.config.ConfigBase;
 import net.minecraft.MethodsReturnNonnullByDefault;
+import net.minecraftforge.common.ForgeConfigSpec;
+import net.nuclearteam.createnuclear.CreateNuclear;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
+import java.util.function.Function;
 
 @MethodsReturnNonnullByDefault
 public class CRadiation extends ConfigBase {
     public final ConfigBool enabledItemRadiation = b(true, "enabledItemRadiation", Comments.enabled);
+    public final ConfiguredList list = nested(0, ConfiguredList::new);
     public final ConfigInt radiationLevel1 = i(10, 0, 50, "radiationLevel1", Comments.radiationLevel1);
     public final ConfigInt radiationLevel2 = i(25, 0, 50, "radiationLevel2", Comments.radiationLevel2);
     public final ConfigInt radiationLevel3 = i(50, 0, 50, "radiationLevel3", Comments.radiationLevel3);
@@ -33,5 +41,52 @@ public class CRadiation extends ConfigBase {
                 + "In Minecraft, amplifier 1 means Radiation II.";
         static String amplifierLevel2 = "Mob effect amplifier used for the third radiation tier. "
                 + "In Minecraft, amplifier 2 means Radiation III.";
+    }
+
+    public static class ConfiguredList extends ConfigBase {
+        private static ForgeConfigSpec.ConfigValue<List<? extends String>> ENTITY_BLACKLIST = null;
+
+        @Override
+        public void registerAll(ForgeConfigSpec.Builder builder) {
+            ENTITY_BLACKLIST = builder
+                    .comment("")
+                    .defineListAllowEmpty(
+                            List.of("entityBlackList"),
+                            () -> List.of(
+                                    "minecraft:armor_stand",
+                                    "minecraft:item_frame",
+                                    "minecraft:glow_item_frame"
+                            ),
+                            obj -> obj instanceof String
+                    );
+
+            builder.pop();
+        }
+
+        @Override
+        public String getName() {
+            return "listValue";
+        }
+
+        public List<? extends String> getEntityBlackList() {
+            return ENTITY_BLACKLIST.get();
+        }
+
+        public static <T> void loadValueInList(List<? extends String> valuesIds, Set<T> targetSet, Function<String, T> func) {
+            targetSet.clear();
+            for (String valueId : valuesIds) {
+                try {
+                    T value = func.apply(valueId);
+
+                    if (value != null) {
+                        targetSet.add(value);
+                    } else {
+                        CreateNuclear.LOGGER.error("Unknown entity type in config: {}", value);
+                    }
+                } catch (Exception e) {
+                    CreateNuclear.LOGGER.error("Unknown entity type in config: {} - {}", valueId, e.getMessage());
+                }
+            }
+        }
     }
 }
