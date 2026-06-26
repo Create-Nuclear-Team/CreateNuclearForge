37b541be201cf98fbbcf4ff35b5d68903f551e68
Giovanni <giovanni.ricotta@ynov.com>
2026-06-27 00:03:41 +0200

    style: import BlockModelBuilder instead of using its fully qualified name
    Replace repeated fully qualified references to
    net.minecraftforge.client.model.generators.BlockModelBuilder in
    PaletteBlockPattern with a proper import. No behavioral change.

--- src/main/java/net/nuclearteam/createnuclear/content/decoration/palettes/PaletteBlockPattern.java
+++ src/main/java/net/nuclearteam/createnuclear/content/decoration/palettes/PaletteBlockPattern.java
@@ -18,6 +18,7 @@ import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraft.core.Direction.Axis;
+import net.minecraftforge.client.model.generators.BlockModelBuilder;
 import net.minecraftforge.client.model.generators.ConfiguredModel;
 import net.nuclearteam.createnuclear.CreateNuclear;
 
@@ -141,7 +142,7 @@ public class PaletteBlockPattern {
     public IBlockStateProvider cubeAll(String variant) {
         ResourceLocation all = toLocation(variant, textures[0]);
         return (ctx, prov) -> {
-            net.minecraftforge.client.model.generators.BlockModelBuilder builder = prov.models()
+            BlockModelBuilder builder = prov.models()
                     .cubeAll(createName(variant), all);
             if (isTranslucent) builder.renderType("translucent");
             prov.simpleBlock(ctx.get(), builder);
@@ -153,7 +154,7 @@ public class PaletteBlockPattern {
         ResourceLocation bottom = toLocation(variant, textures[1]);
         ResourceLocation top = toLocation(variant, textures[2]);
         return (ctx, prov) -> {
-            net.minecraftforge.client.model.generators.BlockModelBuilder builder = prov.models()
+            BlockModelBuilder builder = prov.models()
                     .cubeBottomTop(createName(variant), side, bottom, top);
             if (isTranslucent) builder.renderType("translucent");
             prov.simpleBlock(ctx.get(), builder);
@@ -167,7 +168,7 @@ public class PaletteBlockPattern {
         return (ctx, prov) -> prov.getVariantBuilder(ctx.getEntry())
                 .forAllStatesExcept(state -> {
                             Axis axis = state.getValue(BlockStateProperties.AXIS);
-                            net.minecraftforge.client.model.generators.BlockModelBuilder builder;
+                            BlockModelBuilder builder;
                             if (axis == Axis.Y) {
                                 builder = prov.models().cubeColumn(createName(variant), side, end);
                                 if (isTranslucent) builder.renderType("translucent");
@@ -192,7 +193,7 @@ public class PaletteBlockPattern {
         ResourceLocation side = toLocation(variant, textures[0]);
         ResourceLocation end = toLocation(variant, textures[1]);
         return (ctx, prov) -> {
-            net.minecraftforge.client.model.generators.BlockModelBuilder builder = prov.models()
+            BlockModelBuilder builder = prov.models()
                     .cubeColumn(createName(variant), side, end);
             if (isTranslucent) builder.renderType("translucent");
             prov.simpleBlock(ctx.get(), builder);
