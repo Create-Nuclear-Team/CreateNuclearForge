ab29fee386eb2f60e55254e27bf704a1b6519ff7
Giovanni <giovanni.ricotta@ynov.com>
2026-07-03 21:10:26 +0200

    feat: add shaft placement helper to ReactorOutput block
    Implement IPlacementHelper on ReactorOutput so that right-clicking the
    block while holding a shaft item automatically places the shaft in the
    facing direction. Also annotate the class with @MethodsReturnNonnullByDefault
    and clean up unused imports (Vec3i, BlockEntity, ReactorPattern, etc.).

--- src/main/java/net/nuclearteam/createnuclear/content/multiblock/output/ReactorOutput.java
+++ src/main/java/net/nuclearteam/createnuclear/content/multiblock/output/ReactorOutput.java
@@ -1,24 +1,29 @@
 package net.nuclearteam.createnuclear.content.multiblock.output;
 
 
+import com.simibubi.create.AllBlocks;
 import com.simibubi.create.content.equipment.wrench.IWrenchable;
 import com.simibubi.create.content.kinetics.base.DirectionalKineticBlock;
 import com.simibubi.create.foundation.block.IBE;
+import net.createmod.catnip.placement.IPlacementHelper;
+import net.createmod.catnip.placement.PlacementHelpers;
+import net.createmod.catnip.placement.PlacementOffset;
+import net.minecraft.MethodsReturnNonnullByDefault;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.core.Direction.Axis;
-import net.minecraft.core.Vec3i;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
@@ -29,19 +34,18 @@ import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.VoxelShape;
 import net.nuclearteam.createnuclear.*;
 import net.nuclearteam.createnuclear.content.multiblock.MultiblockHelpers;
-import net.nuclearteam.createnuclear.content.multiblock.controller.ReactorControllerBlock;
 import net.nuclearteam.createnuclear.content.multiblock.controller.ReactorControllerBlockEntity;
-import net.nuclearteam.createnuclear.content.multiblock.pattern.ReactorPattern;
-import net.nuclearteam.createnuclear.foundation.advancement.CNAdvancementBehaviour;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
-import java.util.List;
+import java.util.function.Predicate;
 
 
+@MethodsReturnNonnullByDefault
 public class ReactorOutput extends DirectionalKineticBlock implements IWrenchable, IBE<ReactorOutputEntity> {
-    //public static final IntegerProperty SPEED = IntegerProperty.create("speed", 0, 256);
     public static final IntegerProperty DIR = IntegerProperty.create("dir", 0, 2);
+
+    private static final int placementHelperId = PlacementHelpers.register(new PlacementHelper());
+
 
     public ReactorOutput(Properties properties) {
         super(properties);
@@ -87,6 +91,20 @@ public class ReactorOutput extends DirectionalKineticBlock implements IWrenchabl
         return defaultBlockState().setValue(FACING, preferred)/*.setValue(SPEED, 0)*/.setValue(DIR, 0);
     }
 
+    @Override
+    public InteractionResult use(BlockState state, Level level, BlockPos pos, Player player, InteractionHand hand, BlockHitResult hitResult) {
+        ItemStack heldItem = player.getItemInHand(hand);
+        IPlacementHelper placementHelper = PlacementHelpers.get(placementHelperId);
+        if (!player.isShiftKeyDown() && player.mayBuild()) {
+            if (placementHelper.matchesItem(heldItem) && placementHelper.getOffset(player, level, state, pos, hitResult)
+                .placeInWorld(level, (BlockItem) heldItem.getItem(), player, hand, hitResult)
+                .consumesAction())
+                return InteractionResult.SUCCESS;
+        }
+
+        return super.use(state, level, pos, player, hand, hitResult);
+    }
+
     // IRotate:
 
     @Override
@@ -118,5 +136,29 @@ public class ReactorOutput extends DirectionalKineticBlock implements IWrenchabl
     @Override
     public BlockEntityType<? extends ReactorOutputEntity> getBlockEntityType() {
         return CNBlockEntityTypes.REACTOR_OUTPUT.get();
+    }
+
+    private static class PlacementHelper implements IPlacementHelper {
+
+        @Override
+        public Predicate<ItemStack> getItemPredicate() {
+            return AllBlocks.SHAFT::isIn;
+        }
+
+        @Override
+        public Predicate<BlockState> getStatePredicate() {
+            return s -> s.getBlock() instanceof ReactorOutput;
+        }
+
+        @Override
+        public PlacementOffset getOffset(Player player, Level world, BlockState state, BlockPos pos, BlockHitResult ray) {
+            Direction facing = state.getValue(FACING);
+            BlockPos target = pos.relative(facing);
+
+            if (!world.getBlockState(target).canBeReplaced())
+                return PlacementOffset.fail();
+
+            return PlacementOffset.success(target, s -> s.setValue(BlockStateProperties.AXIS, facing.getAxis()));
+        }
     }
 }
